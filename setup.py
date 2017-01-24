4c55bfe0ceff8a10eb79f42e97001f70873d6c13
nivagator <gavingreer@mac.com>
2017-01-24 10:38:54 -0600

    Cleaned up code in Setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 import sys
 from cx_Freeze import setup, Executable
-import os
-
-os.environ['TCL_LIBRARY'] = "M:\\Applications\\Python\\Python35-32\\tcl\\tcl8.6"
-os.environ['TK_LIBRARY'] = "M:\\Applications\\Python\\Python35-32\\tcl\\tk8.6"
 
 base = None
 if sys.platform == 'win32': base = 'Win32GUI'
